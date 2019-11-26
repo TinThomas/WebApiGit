0929c04ce16605e7da2330f84fd210b637eaf175
Thomas <thomas.baerentsen@gmail.com>
2019-11-26 11:28:30 +0100

    Added some navigation buttons

--- WebApiGit/WebApiGit/Controllers/WeatherForecastController.cs
+++ WebApiGit/WebApiGit/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,18 +12,33 @@ namespace WebApiGit.Controllers
     [Route("[Controller]")]
     public class WeatherForecastController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            Debug.WriteLine("Entering index action");
             return View();
         }
 
         [HttpPost]
         public IActionResult Index(WeatherForecastModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            Debug.WriteLine("Redirecting to success action");
             return RedirectToAction(nameof(Success));
         }
 
+        [Route("Success")]
         public IActionResult Success()
+        {
+            Debug.WriteLine("Entering Success action");
+            return View();
+        }
+
+        [Route("Browse")]
+        public IActionResult Browse()
         {
             return View();
         }
