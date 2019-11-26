cb6a18798be804394a1ff95c4f23d9746030541b
Thomas <thomas.baerentsen@gmail.com>
2019-11-26 08:46:05 +0100

    Added weatherforecastcontroller with index page and action

---
+++ WebApiGit/WebApiGit/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApiGit.Controllers
+{
+    [Route("[Controller]")]
+    public class WeatherForecastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
